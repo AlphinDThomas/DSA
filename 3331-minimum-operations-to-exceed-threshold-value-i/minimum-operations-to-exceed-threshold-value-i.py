6cbb645cdb52e78006815f67429165ca322849a0
Alphin D Thomas <alphinprofessional23@gmail.com>
2026-08-10 17:51:44 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (57.73%) - LeetSync

---
+++ 3331-minimum-operations-to-exceed-threshold-value-i/minimum-operations-to-exceed-threshold-value-i.py
@@ -0,0 +1,7 @@
+class Solution:
+    def minOperations(self, nums: List[int], k: int) -> int:
+        res = []
+        for i in nums:
+            if i>=k:
+                res.append(i)
+        return len(nums)-len(res)
