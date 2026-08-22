df33d8fff3b725e3c880af68eaa4974ddbf8b593
Alphin D Thomas <alphinprofessional23@gmail.com>
2026-08-22 17:55:24 +0530

    Time: 522 ms (8.83%) | Memory: 19.8 MB (11.53%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -1,5 +1,3 @@
-from collections import Counter
-
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
 
@@ -10,32 +8,30 @@ class Solution:
         temp = ""
 
         for right in range(len(s)):
-
+            
             char = s[right]
-            dictesub[char] += 1
-
-            valid = True
+            dictesub[char]+=1
+            
+            valid =  True
 
             for char in dictet:
-                if dictesub[char] < dictet[char]:
+                if dictesub[char]<dictet[char]:
                     valid = False
                     break
 
             while valid:
-
-                substring = s[left:right + 1]
-
-                if temp == "" or len(substring) < len(temp):
-                    temp = substring
-
-                dictesub[s[left]] -= 1
-                left += 1
+                
+                substring = s[left:right+1]
+                if temp == "" or len(substring)<len(temp):
+                    temp =  substring
+                
+                dictesub[s[left]]= dictesub[s[left]]-1
+                left+=1
 
                 valid = True
 
                 for char in dictet:
-                    if dictesub[char] < dictet[char]:
+                    if dictesub[char]<dictet[char]:
                         valid = False
                         break
-
         return temp
