a48050b546e34a4cc904712786ddb394bea26b80
Alphin D Thomas <alphinprofessional23@gmail.com>
2026-08-22 13:24:33 +0530

    Time: 531 ms (8.59%) | Memory: 19.7 MB (74.02%) - LeetSync

---
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -0,0 +1,41 @@
+from collections import Counter
+
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+
+        dictet = Counter(t)
+        dictesub = Counter()
+
+        left = 0
+        temp = ""
+
+        for right in range(len(s)):
+
+            char = s[right]
+            dictesub[char] += 1
+
+            valid = True
+
+            for char in dictet:
+                if dictesub[char] < dictet[char]:
+                    valid = False
+                    break
+
+            while valid:
+
+                substring = s[left:right + 1]
+
+                if temp == "" or len(substring) < len(temp):
+                    temp = substring
+
+                dictesub[s[left]] -= 1
+                left += 1
+
+                valid = True
+
+                for char in dictet:
+                    if dictesub[char] < dictet[char]:
+                        valid = False
+                        break
+
+        return temp
