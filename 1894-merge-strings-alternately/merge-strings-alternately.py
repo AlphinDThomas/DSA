3bfb207211256c633b066b7d2ac4562cd289fffb
Alphin D Thomas <alphinprofessional23@gmail.com>
2026-08-27 18:13:13 +0530

    Time: 47 ms (45.42%) | Memory: 19.2 MB (57.34%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.py
+++ 1894-merge-strings-alternately/merge-strings-alternately.py
@@ -1,21 +1,22 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        
+        len1 = len(word1)
+        len2 = len(word2)
         i = 0
         j = 0
         substring = ""
-        while i<len(word1) and j<(len(word2)):
+        while i<len1 and j<len2:
             substring += word1[i]
             substring += word2[j]
             i+=1
             j+=1
         
-        if i<len(word1):
-            while i<len(word1):
+        if i<len1:
+            while i<len1:
                 substring += word1[i]
                 i+=1
-        if j<len(word2):
-            while j<len(word2):
+        if j<len2:
+            while j<len2:
                 substring += word2[j]
                 j+=1
         return substring
