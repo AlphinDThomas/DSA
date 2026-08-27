9ed02be0f1028d4b9bc75c6f564c854b5a3392ba
Alphin D Thomas <alphinprofessional23@gmail.com>
2026-08-27 17:46:48 +0530

    Time: 27 ms (51.85%) | Memory: 25.5 MB (100.00%) - LeetSync

--- 2113-find-the-kth-largest-integer-in-the-array/find-the-kth-largest-integer-in-the-array.py
+++ 2113-find-the-kth-largest-integer-in-the-array/find-the-kth-largest-integer-in-the-array.py
@@ -1,16 +1,11 @@
 class Solution:
     def kthLargestNumber(self, nums: List[str], k: int) -> str:
         
-        list1 = []
-        for i in nums:
-            list1.append(int(i))
-
-        def bubblesort(nums):
-            for i in range(len(nums)):
-                for j in range(len(nums)-i-1):
-                    if nums[j]>nums[j+1]:
-                        nums[j],nums[j+1] = nums[j+1],nums[j]
-            return nums
+        for i in range(len(nums)):
+            nums[i] = int(nums[i]) * -1
         
-        list1.sort()
-        return str(list1[-k])
+        heapq.heapify(nums)
+
+        for _ in range(k-1):
+            heapq.heappop(nums)
+        return str(-heapq.heappop(nums))
