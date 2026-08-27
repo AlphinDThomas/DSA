e8935ed401e7772bac20e6907eef774086b932c0
Alphin D Thomas <alphinprofessional23@gmail.com>
2026-08-27 17:43:41 +0530

    Time: 19 ms (84.92%) | Memory: 26.4 MB (42.86%) - LeetSync

--- 2113-find-the-kth-largest-integer-in-the-array/find-the-kth-largest-integer-in-the-array.py
+++ 2113-find-the-kth-largest-integer-in-the-array/find-the-kth-largest-integer-in-the-array.py
@@ -5,6 +5,12 @@ class Solution:
         for i in nums:
             list1.append(int(i))
 
+        def bubblesort(nums):
+            for i in range(len(nums)):
+                for j in range(len(nums)-i-1):
+                    if nums[j]>nums[j+1]:
+                        nums[j],nums[j+1] = nums[j+1],nums[j]
+            return nums
+        
         list1.sort()
-
         return str(list1[-k])
