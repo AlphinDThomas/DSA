db19ebf8519327a4d386b80bae3b976f0aacfd9a
Alphin D Thomas <alphinprofessional23@gmail.com>
2026-08-28 09:18:38 +0530

    Time: 466 ms (58.10%) | Memory: 32.4 MB (12.59%) - LeetSync

--- 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.py
+++ 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.py
@@ -5,19 +5,13 @@ class Solution:
         pairs = 0
         
         for num in nums:
-            if (k-num) in dicte:
-                if dicte[k-num]>0:
-                    pairs += 1
-                    dicte[k-num] -= 1
-                elif dicte[k-num]==0:
-                    if num in dicte:
-                        dicte[num]+=1
-                    else:
-                        dicte[num] = 1
-            elif (k-num) not in dicte:
+            if (k-num) in dicte and dicte[k-num]>0:
+                pairs += 1
+                dicte[k-num] -= 1
+            else:
                 if num in dicte:
                     dicte[num]+=1
                 else:
-                    dicte[num] = 1
+                    dicte[num]=1
         return pairs
             
