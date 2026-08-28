0b82290b4e8fbd0ac1162e4f3ad85d1b4a1d8c03
Alphin D Thomas <alphinprofessional23@gmail.com>
2026-08-28 15:19:16 +0530

    Time: 40 ms (97.35%) | Memory: 20.8 MB (10.06%) - LeetSync

--- 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.py
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.py
@@ -1,6 +1,6 @@
 class Solution:
     def closeStrings(self, word1: str, word2: str) -> bool:
-        
+        '''
         if len(word1)!=len(word2):
             return False
         word1 = list(word1)
@@ -31,4 +31,17 @@ class Solution:
             return False
         
         return True
-        
+        '''
+
+        if len(word1)!=len(word2):
+            return False
+        dicte1 = dict()
+        dicte2 = dict()
+        for i in word1:
+            if i not in dicte1:
+                dicte1[i] = word1.count(i)
+        for j in word2:
+            if j not in dicte2:
+                dicte2[j] = word2.count(j)
+
+        return dicte1.keys() == dicte2.keys() and sorted(dicte1.values()) == sorted(dicte2.values())
