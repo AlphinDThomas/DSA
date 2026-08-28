0bc0c7cec0d01b53ede91173e8d3d092ead987c2
Alphin D Thomas <alphinprofessional23@gmail.com>
2026-08-28 16:45:51 +0530

    Time: 7 ms (57.41%) | Memory: 20.2 MB (92.77%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.py
+++ 735-asteroid-collision/asteroid-collision.py
@@ -2,48 +2,27 @@ class Solution:
     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
         
         stack = []
-        def recur(num1,num2):
-                val1 = abs(num2)
-                val2 = abs(num1)
-                while len(stack)>=2:
-                    if val1 == val2:
-                        stack.pop()
-                        stack.pop()
-                        return
-                    elif val1<val2:
-                        stack.pop()
-                        stack.pop()
-                        stack.append(num1)
-                        if len(stack)>=2:
-                            if stack[-1]<0 and stack[-2]>0:
-                                recur(stack[-1],stack[-2])
-                        return
-                    elif val1>val2:
-                        stack.pop()
-                        return
-                    
-                    
+        
+        for ast in asteroids:
+            survived = True
+            while stack and stack[-1]>0 and ast<0:
+                
+                if stack[-1]<abs(ast):
+                    stack.pop()
                         
                     
-        
-        for i in asteroids:
-            if not stack:
-                stack.append(i)
-            elif stack:
+                elif stack[-1] == abs(ast):
+                    stack.pop()
+                    survived = False
+                    break
                 
-                if stack[-1]<0 and i<0:
-                    stack.append(i)
-                elif stack[-1]>0 and i>0:
-                    stack.append(i)
-                elif stack[-1]<0 and i>0:
-                    stack.append(i)
                 else:
-                    stack.append(i)
-                   
-                    num1 = stack[-1]
-                    num2 = stack[-2]
+                    survived = False
+                    break
+                
+            if survived:
+                stack.append(ast)
 
-                    recur(num1,num2)
         return stack
         
                 
