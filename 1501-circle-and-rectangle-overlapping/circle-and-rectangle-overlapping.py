3a556cf77ddff2ce8d5101914a0f4c045ee6f688
Alphin D Thomas <alphinprofessional23@gmail.com>
2026-09-19 15:01:17 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (18.67%) - LeetSync

---
+++ 1501-circle-and-rectangle-overlapping/circle-and-rectangle-overlapping.py
@@ -0,0 +1,10 @@
+class Solution:
+    def checkOverlap(self, radius: int, xCenter: int, yCenter: int, x1: int, y1: int, x2: int, y2: int) -> bool:
+        
+        closestX = max(x1,min(x2,xCenter))
+        closestY = max(y1,min(y2,yCenter))
+
+        distance = (closestX-xCenter)**2 + (closestY-yCenter)**2
+        if distance<=radius**2:
+            return True
+        return False
